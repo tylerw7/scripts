da538ab1b1702f117f8f868d9640610ffe568f86
Tyler Webster <twebster137@gmail.com>
2020-02-16 14:35:32 -0600

    Use pamixer toggle option.

--- tog_vol.sh
+++ tog_vol.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # toggle volumte mute
 
-if [[ $(pamixer --get-mute) == "true" ]]; then
-	pamixer -u
-else
-	pamixer -m
-fi
+# if [[ $(pamixer --get-mute) == "true" ]]; then
+	# pamixer -u
+# else
+	# pamixer -m
+# fi
 
+paxmier --toggle
 pkill -SIGRTMIN+9 i3blocks
 
